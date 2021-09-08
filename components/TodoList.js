b0223bbf6d8c2cd7329e9e09992a7109f4994262
WIN10-01\UserK <jchan.pref@gmail.com>
2021-09-08 17:08:02 +0900

    'todo'

---
+++ components/TodoList.js
@@ -0,0 +1,44 @@
+import React, {useEffect} from 'react';
+import {StyleSheet, ScrollView, Text, FlatList} from 'react-native';
+import TodoListItem from './TodoListItem';
+
+const TodoList = ({todos, onRemove, onToggle}) => {
+  const renderListEl = ({item, index}) => {
+    // console.log(`RenderListEl >>>> ${item.id}`);
+    return <TodoListItem todo={item} onRemove={onRemove} onToggle={onToggle} />;
+  };
+
+  useEffect(() => {
+    console.log('todos ---- ');
+    console.log(todos);
+  }, [todos]);
+
+  return (
+  //   <ScrollView contentContainerStyle={styles.listContainer}>
+  //    {todos.map(todo => {
+  //     return (
+  //       <TodoListItem
+  //         key={todo.id}
+  //         {...todo}
+  //         onRemove={onRemove}
+  //         onToggle={onToggle}
+  //       />
+  //     );
+  //   })}
+  // </ScrollView>
+
+    <FlatList
+      data={todos}
+      renderItem={renderListEl}
+      keyExtractor={item => item.id}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  listContainer: {
+    alignItems: 'center',
+  },
+});
+
+export default TodoList;
